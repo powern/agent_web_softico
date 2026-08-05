c072135d08fa11f8a7373bbf89f67f1c2be4daf6
powern <powern76@gmail.com>
2026-08-05 09:53:46 +0200

    Use refined public backup scanner

--- src/wp_guardian/audit.py
+++ src/wp_guardian/audit.py
@@ -5,8 +5,9 @@ import json
 from .config import GuardianConfig
 from .discovery import discover_sites
 from .models import Site, SiteAudit
+from .public_backups import scan_public_backups
 from .runner import CommandRunner
-from .scanner import scan_public_backups, scan_uploads, scan_world_writable
+from .scanner import scan_uploads, scan_world_writable
 from .storage import Storage
 from .wordpress import WordPress
 
